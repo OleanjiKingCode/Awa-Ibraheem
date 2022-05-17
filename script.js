fc570d86aa0cf3df70f0ed0fd4f1aba432ec8ee4
OleanjiKingCode <holamilean56@gmail.com>
2022-05-17 14:47:04 +0100

    yo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sliders2 = document.querySelector(".slide2-in");
 
 const appearOptions = {
     threshold: 0,
-    rootMargin: "20px"
+    rootMargin: "10px"
   };
   
   const appearOnScroll = new IntersectionObserver(function(
