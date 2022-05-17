f2dd9a089f87f37e562c5f9b6607616a3665ca3e
OleanjiKingCode <holamilean56@gmail.com>
2022-05-17 14:19:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const sliders = document.querySelectorAll(".slide-in");
+const sliders2 = document.querySelector(".slide2-in");
+
+
+const appearOptions = {
+    threshold: 0.5,
+    rootMargin: "0px 0px -200px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
+
+
+  const appearOptions2 = {
+    root:null,
+    threshold: 1,
+    rootMargin: "0px"
+ 
+  };
+  
+  const appearOnScroll2 = new IntersectionObserver(function(
+    entries,
+    appearOnScroll2
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll2.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+
+ appearOnScroll2.observe(sliders2);
+ 
+
+
+    $("#partners .owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        dots:false,
+        nav:true
+    });
